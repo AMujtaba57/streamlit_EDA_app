cf76048632586c4de191384d64c0561948537a12
Ahmad Mujtaba <2017cs401@student.uet.edu.pk>
2023-03-13 20:13:44 +0500

    first_part

---
+++ utils/utils.py
@@ -0,0 +1,47 @@
+import warnings
+warnings.filterwarnings('ignore')
+import numpy
+from numpy import arange
+from matplotlib import pyplot
+from pandas import read_csv
+from pandas import set_option
+from pandas.plotting import scatter_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.metrics import mean_squared_error
+
+
+def read_data(filename):
+    names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+    return read_csv(filename, delim_whitespace=True, names=names)
+
+def missing_value(filename):
+    names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+    dataframe = read_csv(filename, delim_whitespace=True, names=names)
+    missing_values = dataframe.isnull()
+    return missing_values.sum()
+
+def show_datatype(filename):
+    names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+    dataframe = read_csv(filename, delim_whitespace=True, names=names)
+    return dataframe.dtypes
+
+def descriptive_search(filename):
+    names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+    dataframe = read_csv(filename, delim_whitespace=True, names=names)
+    return dataframe.describe()
+
